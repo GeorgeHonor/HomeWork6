fe95a70f63d898d10d850ebb01e6dca07f205703
Георгий <saygora@gmail.com>
2022-01-16 18:55:22 +0300

    ДЗ№6

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,31 +5,19 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        //Ex.№1
         int[] arr = generateRandomArray();
-
-    }
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-        }
-        //System.out.print(Arrays.toString(arr));
-
-        {
-            //Ex.№1
-            System.out.println();
-            int sum = 0;
-            for (int element : arr) {
+        System.out.println();
+        int sum = 0;
+        for (int element : arr) {
             sum = sum + element;
-            }
-            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
-            }
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 
-            //Ex.№2
-            int min = arr[0];
-            int max = arr[0];
-          for (int arrs : arr) {
+        //Ex.№2
+        int min = arr[0];
+        int max = arr[0];
+        for (int arrs : arr) {
             if (arrs < min) {
                 min = arrs;
             }
@@ -41,7 +29,6 @@ public class Main {
         System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
 
         //Ex.№3
-        int sum = 0;
         for (int element : arr) {
             sum = sum + element;
         }
@@ -56,11 +43,15 @@ public class Main {
         for (int i = reverseFullName.length - 1; i >= 0; i-- ) {
             System.out.print(reverseFullName[i]);
         }
-            return arr;}
 
-
-
-
+}
+        public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;}
 
     }
 
